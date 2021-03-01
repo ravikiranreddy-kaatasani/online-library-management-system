5f9aeb37aec490d73d0bc1e9d3c7ef1359abbedb
sitaramarao-v-s-v-s <76678705+sitaramarao-v-s-v-s@users.noreply.github.com>
2021-03-01 10:37:35 +0530

    Update admin_sign_up.py

--- admin_sign_up.py
+++ admin_sign_up.py
@@ -1,25 +1,37 @@
 import pymysql
 import base64
-from re import fullmatch
+import re
 
-mydb=pymysql.connect(host='localhost',user='root',password='root',database='library_management_trial')
-cursor=mydb.cursor()
+mydb = pymysql.connect(host='localhost', user='root', password='root', database='library_mgmt')
+cursor = mydb.cursor()
 
-admin_name=input('enter user name').strip()
-admin_mail= input('enter user mail').strip()
-password= input('enter passwordd').strip()
+admin_name = input('enter user name ').strip()
+admin_mail = input('enter user mail ').strip()
+password = input('enter password ').strip()
 
-def user_signup(admin_name,admin_mail,password):
-    if cursor.execute("select user_mail_id from user_tt where user_mail_id =%s;",(admin_mail)) == 1:
-        print('user already exists')
-    else:
+def check(mail):
+    mail = mail.strip()
+    pattern = "(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+    res = re.match(pattern,mail)
+    return  res
 
-        if fullmatch("(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{8,20}",password):
-            encoded_pwd = base64.b64encode(password.encode('ascii'))
-            cursor.execute("insert into user_tt (user_name,user_mail_id,password,is_admin_flag) values(%s,%s,%s,%s)",(admin_name,admin_mail,encoded_pwd,1))
-            mydb.commit()
-            #print('hi')
+def user_signup(admin_name, admin_mail, password):
+    try:
+        if cursor.execute("select user_mail_id from users where user_mail_id =%s;", (admin_mail)) == 1:
+            print('user already exists')
         else:
-            print("--Password did not match the requirements--")
-user_signup(admin_name,admin_mail,password)
+            if check(admin_mail):
+                if fullmatch("(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{8,20}", password):
+                    encoded_pwd = base64.b64encode(password.encode('ascii'))
+                    cursor.execute("insert into user_tt (user_name,user_mail_id,password,is_admin_flag) values(%s,%s,%s,%s)",
+                                   (admin_name, admin_mail, encoded_pwd, 1))
+                    mydb.commit()
+                    print('hi')
+                else:
+                    print("--Password did not match the requirements--")
+            else:
+                print("Input valid mail id")
+    except Exception as e:
+        print("Error",e)
 
+user_signup(admin_name, admin_mail, password)
